0216fa45ec50eb4a4f6183d9ad7d988d96bc55a1
cloudopsdojo <cloudopsdojo@gmail.com>
2026-02-19 10:43:01 +0530

    added more food items

--- src/controllers/food.controller.js
+++ src/controllers/food.controller.js
@@ -36,6 +36,36 @@ export const seedFoods = async (req, res) => {
       ],
         isVeg: false,
         country: "India"
+      },
+      {
+        name: "Veg Noodles",
+        description: "A staple dish in Chinese cuisine made from wheat or rice flour. Stir-fried with vegetables, sauces, and sometimes meat, noodles are flavorful and versatile.",
+        images: [
+        "/images/noodles_1.jpg",
+        "/images/noodles_2.jpg",
+      ],
+        isVeg: false,
+        country: "CN"
+      },
+      {
+        name: "Chicken Burger",
+        description: "A sandwich consisting of a grilled patty placed inside a bun with lettuce, tomato, cheese, and sauces,meat-based.",
+        images: [
+        "/images/burger_beef_1.jpg",
+        "/images/burger.avif",
+      ],
+        isVeg: true,
+        country: "US"
+      },
+      {
+        name: "Veg Pizza",
+        description: "An Italian flatbread topped with tomato sauce, cheese, and various toppings and Chicken",
+        images: [
+        "/images/veg_pizza_1.jpg",
+        "/images/veg_pizza_2.jpg",
+      ],
+        isVeg: false,
+        country: "IT"
       }
     ];
 
